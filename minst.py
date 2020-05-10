ce7d1fffb1359f41553a938efa9ef718bdfb61ea
HeShuHeng <1072436890@qq.com>
2020-05-10 23:58:35 +0800

    file related function in minst.py

--- minst.py
+++ minst.py
@@ -157,15 +157,14 @@ def load_test_labels(idx_ubyte_file=test_labels_idx1_ubyte_file):
 
 if __name__ == '__main__':
     train_images = load_train_images()
-
     train_labels = load_train_labels()
     # test_images = load_test_images()
     # test_labels = load_test_labels()
 
     # 查看前十个数据及其标签以读取是否正确
     for i in range(10):
-        print(train_labels[i])
-        plt.imshow(train_images[i], cmap='gray')
+        print(train_labels[i+10])
+        plt.imshow(train_images[i+10], cmap='gray')
         plt.pause(0.000001)
         plt.show()
     print('done')
